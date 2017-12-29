8d8079911d76d8cb2a94fe4f87ee2a3266ba7842
Thai Pangsakulyanont <dtinth@spacet.me>
2017-12-29 13:55:25 +0700

    :lock: Early error when testing the app without specifying docs to generate

--- nwb.config.js
+++ nwb.config.js
@@ -2,6 +2,13 @@ require('ts-node/register')
 let data
 
 module.exports = function ({ args }) {
+  let rootFileNames = (args.doc || { }).input
+  if (!rootFileNames) {
+    console.error('Please specify the file names to generate the docs.')
+    console.error('')
+    console.error('    --doc.input src/index.js')
+    process.exit(1)
+  }
   return {
     devServer: {
       before (app) {
@@ -9,13 +16,6 @@ module.exports = function ({ args }) {
           if (!data) {
             console.log('Generating documentation data...')
             const generateDocs = require('./src/generator/generateDocs').default
-            let rootFileNames = (args.doc || { }).input
-            if (!rootFileNames) {
-              console.error('Please specify the file names to generate the docs.')
-              console.error('')
-              console.error('    --doc.input src/index.js')
-              process.exit(1)
-            }
             if (!Array.isArray(rootFileNames)) {
               rootFileNames = [ String(rootFileNames) ]
             }
