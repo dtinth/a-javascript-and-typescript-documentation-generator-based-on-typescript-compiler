f2dcd2ea1d73e02383a25a658bc4d4df50258583
Thai Pangsakulyanont <dtinth@spacet.me>
2017-12-29 13:55:40 +0700

    :sparkles: componentDidCatch

--- src/web/dev/App.js
+++ src/web/dev/App.js
@@ -1,15 +1,37 @@
 import generateDocumentationSite, { renderPage } from '../generateDocumentationSite'
 
 import React from 'react'
-import data from '/tmp/redux.json'
 import enableHotReload from 'enable-hot-reload'
-import styled from 'styled-components'
 
 const hot = enableHotReload(module)
 
-function App () {
-  const site = generateDocumentationSite(data)
-  return renderPage(site.pages[0])
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { data: 'Loading...' }
+  }
+  async componentDidMount () {
+    try {
+      const json = await window.fetch('/docs-data').then(res => res.json())
+      const site = generateDocumentationSite(json)
+      this.setState({ data: site })
+    } catch (e) {
+      this.setState({ data: e.stack })
+      setTimeout(() => { throw e })
+    }
+  }
+  componentDidCatch (e) {
+    this.setState({ data: e.stack })
+    setTimeout(() => { throw e })
+  }
+  render () {
+    const { data } = this.state
+    if (typeof data === 'string') {
+      return <pre style={{ whiteSpace: 'pre-wrap', fontSize: 20, padding: 10, fontFamily: 'Comic Sans MS, sans-serif', textShadow: '0 0 5px #aaffaa, 0 0 10px #aaffaa' }}>{data}</pre>
+    }
+    const site = data
+    return renderPage(site.pages[0])
+  }
 }
 
 export default hot(React, App)
